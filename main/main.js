4a02e46fbe15332f54f6f93e1a12f5ef03d92173
fckingcryxanz <fckingcrzyxanz@gmail.com>
2026-06-06 02:16:48 +0500

    Create main.js

---
+++ main/main.js
@@ -0,0 +1,15 @@
+// public/modules/main/main.js
+export const MainModule = {
+    init: () => {
+        console.log("Main Module Loaded");
+    },
+    // Функция для создания стандартных карточек в стиле iOS
+    createCard: (title, content) => {
+        return `
+            <div class="glass-card">
+                <h3>${title}</h3>
+                <div class="card-body">${content}</div>
+            </div>
+        `;
+    }
+};
